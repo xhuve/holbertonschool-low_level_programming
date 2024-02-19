99e4d1bdd95847dd467415772c0161bdd0155e22
xhuve <xhuvelialbana@gmail.com>
2024-02-19 13:26:49 +0100

    Fix some exercises

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char is: %li byte(s)\n", sizeof(char));
-	printf("Size of a int is: %li byte(s)\n", sizeof(int));
-	printf("Size of a long int is: %li byte(s)\n", sizeof(long int));
-	printf("Size of a long long int is: %li byte(s)\n", sizeof(long long int));
-	printf("Size of a float is: %li byte(s)\n", sizeof(float));
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of a int: %li byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %li byte(s)\n", sizeof(float));
 
 	return (0);
 }
