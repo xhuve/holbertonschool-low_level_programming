f8b5d00e54be7c62621fc9ff965e24639e972864
xhuve <xhuvelialbana@gmail.com>
2024-02-20 02:19:34 -0800

    initial commit of new projekt

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ *
+ *
+ * Return:  0 on success. Error code otherwise
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
