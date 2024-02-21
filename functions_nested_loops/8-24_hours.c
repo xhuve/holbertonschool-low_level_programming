8c6bec9b4668a0ba9558192ca3640e3a3162b672
xhuve <xhuvelialbana@gmail.com>
2024-02-21 11:02:58 +0100

    Test exercise

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - Program entry point
+ *
+ *
+ * Return:  0 on success. Error code otherwise
+ */
+
+void jack_bauer(void)
+{
+	char i, j, l, m;
+	
+	for (i = 48; i <= 50; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (l = 48; l < 54; l++)
+			{
+				for (m = 48;  m <= 57; m++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(l);
+					_putchar(m);
+					_putchar('\n');
+					if (i == 50 && j == 51 && l == 53 && m == 57)
+					{
+						return;
+					}
+				}
+			}
+		}
+	}
+}
