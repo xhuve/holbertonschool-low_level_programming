fe55f873257624cd0a5f084842bdac560167ba72
xhuve <xhuvelialbana@gmail.com>
2024-02-21 11:04:28 +0100

    Test betty coding style

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Program entry point
+ * jack_bauer - Program entry point
  *
  *
  * Return:  0 on success. Error code otherwise
@@ -10,7 +10,7 @@
 void jack_bauer(void)
 {
 	char i, j, l, m;
-	
+
 	for (i = 48; i <= 50; i++)
 	{
 		for (j = 48; j <= 57; j++)
