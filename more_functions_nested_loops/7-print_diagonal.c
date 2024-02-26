feea6e8c9092c469bf98106ca2b96046d25fba7e
xhuve <xhuvelialbana@gmail.com>
2024-02-26 14:50:45 +0100

    Fix

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,7 +12,7 @@ void print_diagonal(int n)
 {
 	int space, slash;
 
-	if (n <= 10)
+	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; ++i)
 	{
 		if (i % 3 == 0)
 		{
@@ -24,7 +24,8 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		printf(" ");
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
