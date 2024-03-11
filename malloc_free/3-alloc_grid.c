eee92e904ac331afe94bd0e15185977d96eb2a19
xhuve <xhuvelialbana@gmail.com>
2024-03-11 15:26:13 +0100

    Fix 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,13 +19,20 @@ int **alloc_grid(int width, int height)
 
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
+	{
+		free(grid);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			free(grid[i]);
+			free(grid);
 			return (NULL);
+		}
 	}
 
 
