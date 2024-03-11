ffdb955dea8a4b991305e78bd5f1d827ccfca9f2
xhuve <xhuvelialbana@gmail.com>
2024-03-11 15:59:35 +0100

    Fix 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,12 +14,15 @@ char *_strdup(char *str)
 	int i = 0, len = 0;
 
 	if (str == NULL)
+		free(dup);
 		return (NULL);
 
 	while (str[len] != '\0')
 		len++;
 
-	dup = malloc(sizeof(char) * len);
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
 
 	for (i = 0; i < len; i++)
 		dup[i] = str[i];
