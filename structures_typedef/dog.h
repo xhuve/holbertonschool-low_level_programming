bf5adbe0562787e2726ecf67c3e39855b0dbce60
xhuve <xhuvelialbana@gmail.com>
2024-03-13 13:54:56 +0100

    fix dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,6 @@
 
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - check the code
  * @name: name.
@@ -7,17 +9,16 @@
  *
  * Return: Always 0.
  */
-#ifndef dog
-#define dog
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-#endif
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+/*
+* dog_t *new_dog(char *name, float age, char *owner);
+* void free_dog(dog_t *d);
 */
+#endif
