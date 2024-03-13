f3c6389209c6cdcccaf07d9cd92bbed469490f9b
xhuve <xhuvelialbana@gmail.com>
2024-03-13 13:34:36 +0100

    Fix 1 n 2 exercise

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,12 +3,18 @@
 
 /**
  * init_dog - check the code
+ * @d: struct dog
+ * @name: characters
+ * @age: int
+ * @owner: owner
  *
  * Return: Always 0.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 
 	d->name = name;
 	d->age = age;
--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * print_dog - check the code
- * @dog d: test.
+ * @d: test.
  *
  * Return: Always 0.
  */
@@ -13,10 +13,10 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	d->name ? printf("Name: %s", d->name) : printf("Name: NULL");
+	d->name ? printf("Name: %s", d->name) : printf("Name: (nil)");
 	printf("\n");
-	d->age ? printf("Age: %f", d->age) : printf("Age: NULL");
+	d->age < 0 ? printf("Age: %f", d->age) : printf("Age: (nil)");
 	printf("\n");
-	d->owner ? printf("Owner: %s", d->owner) : printf("Owner: NULL");
+	d->owner ? printf("Owner: %s", d->owner) : printf("Owner: (nil)");
 	printf("\n");
 }
