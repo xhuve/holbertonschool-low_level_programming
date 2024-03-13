95416952e188cf81c977afb28a78d1fb1ff7d3fd
xhuve <xhuvelialbana@gmail.com>
2024-03-13 21:00:51 +0100

    Fix 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,6 +17,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return NULL;
 
+	d->name = malloc(sizeof(d->name));
+	if(d->name == NULL)
+		return NULL;
+
+	d->owner = malloc(sizeof(d->owner));
+	if(d->owner == NULL)
+		return NULL;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
