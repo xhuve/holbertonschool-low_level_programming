0d5a284a019dbdbf00058b55ebca50200f982e17
xhuve <xhuvelialbana@gmail.com>
2024-03-13 14:52:23 +0100

    Finish last set of exercises

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - check the code
+ * @name: test.
+ * @age: number.
+ * @owner: owner/
+ *
+ * Return: Always 0.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return NULL;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - check the code
+ * @d: owner.
+ *
+ * Return: Always 0.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
