a61fb2ccedab4c29fc6800e1c811373858fafed9
xhuve <xhuvelialbana@gmail.com>
2024-03-15 13:20:17 +0100

    If this isnt it idk what is

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-#include <string.h>
 
 /**
  * new_dog - check the code
@@ -11,26 +10,41 @@
  *
  * Return: Always 0.
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *namec = malloc(strlen(name) + 1);
-	char *ownerc = malloc(strlen(owner) + 1);
+	int len1 = 0, len2 = 0;
+	char *namec, *ownerc;
 	dog_t *d = malloc(sizeof(dog_t));
-	
-	if (d == NULL || namec == NULL || ownerc == NULL)
+
+	if (d == NULL)
+		return (NULL);
+
+	while (name[len1] != '\0')
+		len1++;
+	namec = malloc(len1 + 1);
+	while (owner[len2] != '\0')
+		len2++;
+	ownerc = malloc(len2 + 1);
+
+	if (ownerc == NULL || namec == NULL)
 	{
 		free(namec);
 		free(ownerc);
-		free(d);
 		return (NULL);
 	}
-	strcpy(namec, name);
-	strcpy(ownerc, owner);
+
+	namec[len1] = '\0';
+	while (len1-- > 0)
+		namec[len1] = name[len1];
+
+	ownerc[len2] = '\0';
+	while (len2-- > 0)
+		ownerc[len2] = owner[len2];
 
 	d->name = namec;
 	d->age = age;
 	d->owner = ownerc;
-
 	return (d);
 }
+
+
