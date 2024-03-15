eccd7f7f2901b9b0e30771fea56bc13c81881817
xhuve <xhuvelialbana@gmail.com>
2024-03-15 13:21:19 +0100

    If this isnt it idk what is

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,7 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d = malloc(sizeof(dog_t));
 
 	if (d == NULL)
+	{
+		free(d);
 		return (NULL);
+	}
 
 	while (name[len1] != '\0')
 		len1++;
