c3bd7f3a5ce9b7d8e9ff949c7d14e81e6495bc85
xhuve <xhuvelialbana@gmail.com>
2024-03-18 15:10:50 +0100

    fix 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,41 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * main - check code
- * @argc: int
- * @argv: array
- *
- * Return: thing
+ * main - Entry point. Performs basic arithmetic operations on two numbers.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0 (Success).
  */
 
 int main(int argc, char *argv[])
 {
-	int (*func)(int, int);
-	char *arg = argv[2];
+	int num1, num2;
+	int (*op)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	if (atoi(argv[3]) == 0 && (arg[0] == '/' || arg[0] == '%'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	func = get_op_func(arg);
-
-	if (!func || arg[1] != '\0')
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = get_op_func(argv[2]);
+	if (op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-
-	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", op(num1, num2));
 	return (0);
 }
