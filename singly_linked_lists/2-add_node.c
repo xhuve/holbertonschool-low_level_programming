18d8ce210bf72e6eb9ea9ae22fa1f9b2b526c65c
xhuve <xhuvelialbana@gmail.com>
2024-04-03 16:58:48 +0200

    Remove strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,12 +10,18 @@
 * @str: str of node
 * Return: addr
 */
+
+
+
 list_t *add_node(list_t **head, const char *str)
 {
+	unsigned int i;
 	list_t *new = malloc(sizeof(list_t));
 
+	for (i = 0; str[i] != '\0'; i++);
+
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = i;
 	new->next = *head;
 	*head = new;
 
