0ead810cef6a184cdc954ae25a86ebb40c30db5c
xhuve <xhuvelialbana@gmail.com>
2024-04-05 01:12:28 +0200

    Finish 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* print_list - check code
+*
+* @h: head of node
+* Return: length of node
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
+
