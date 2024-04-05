539cee74323e8ac496feb311d300d0eb00b82b40
xhuve <xhuvelialbana@gmail.com>
2024-04-05 02:19:14 +0200

    Finish 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_dlistint - check code
+*
+* @h: head of node
+* Return: length of node
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr;
+
+	while (head != NULL)
+	{
+		curr = head->next;
+		free(head);
+		head = curr;
+	}
+
+}
+
