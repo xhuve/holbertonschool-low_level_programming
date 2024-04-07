204bcae8301e178f02c69b13e7bf8556248f5630
xhuve <xhuvelialbana@gmail.com>
2024-04-07 17:28:54 +0200

    Finish 8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* insert_dnodeint_at_index - check code
+*
+* @h: head of node
+* @index: value of node
+* Return: length of node
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp;
+	unsigned int i;
+
+	if (*head != NULL)
+		tmp = *head;
+	else
+		return (-1);
+
+	if (index == 0)
+	{
+		tmp->prev = NULL;
+		*head = tmp->next;
+		return (0);
+	}
+	printf("not 0");
+	for (i = 0; i < index - 1; i++)
+	{
+		if (tmp->next == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	tmp->prev = tmp->next; 
+	tmp->next = tmp->next->next;
+
+	return (0);
+}
+
