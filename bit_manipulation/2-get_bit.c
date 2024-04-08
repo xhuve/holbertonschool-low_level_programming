2c2b6f343b18d4c0c3675b9cf272b1360e6ea576
xhuve <xhuvelialbana@gmail.com>
2024-04-08 12:02:13 +0200

    Fix 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,9 +12,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int size;
 
-	size = malloc(sizeof(unsigned long int));
+	size = sizeof(n) * 8;
 
 	if (index > size)
 		return (-1);
+
 	return (n >> index) & 1;
 }
