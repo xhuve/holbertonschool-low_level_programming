801692f2b9af3dd26a1f584050410a3d4e74b733
xhuve <xhuvelialbana@gmail.com>
2024-04-08 13:18:17 +0200

    Finish 4

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* clear_bit - check code
+*
+* @n: number
+* @index: length
+* Return: number
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int size = 0;
+
+	size = sizeof(n) * 8;
+	if (index > size)
+		return (-1);
+
+
+	*n = *n & (~ 1 << index);
+
+	return (1);
+}
