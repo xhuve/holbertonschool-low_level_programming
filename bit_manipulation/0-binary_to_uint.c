8ef56a4c03739601daca1db1013e73b60161a7ff
xhuve <xhuvelialbana@gmail.com>
2024-04-08 10:45:38 +0200

    Finish 1 exercise

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	int count;
+	unsigned int sum = 0, pow = 1;
+
+	if (b == NULL)
+		return (0);
+
+	for (count = 0; b[count] != '\0'; count++)
+	{
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+	}
+
+	for(count -= 1; count >= 0; count--)
+	{
+		if (b[count] == '1')
+			sum += pow; 
+		pow *= 2;
+	}
+
+	return (sum);
+}
+
+
---
+++ bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Learning about bit maniulation, very cool
