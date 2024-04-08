5f496d4625fb5966bcab95706f62d8425fbc3be1
xhuve <xhuvelialbana@gmail.com>
2024-04-08 11:56:55 +0200

    Possible solution for 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.c>
 #include "main.h"
 
+/**
+* get_bit - check code
+*
+* @n: number
+* @index: length
+*/
 int get_bit(unsigned long int n, unsigned int index)
 {
-	
+	unsigned long int size;
 
+	size = malloc(sizeof(unsigned long int));
+
+	if (index > size)
+		return (-1);
 	return (n >> index) & 1;
 }
