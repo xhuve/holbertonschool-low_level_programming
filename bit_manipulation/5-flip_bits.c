87d1485225aaf0076cb83ab954a4f837d5bfe20a
xhuve <xhuvelialbana@gmail.com>
2024-04-08 15:25:15 +0200

    Finish 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* flip_bits - check code
+*
+* @n: number
+* @index: length
+* Return: number
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int result;
+	unsigned int count = 0;
+
+	result = n ^ m;
+
+	while (result != 0)
+	{
+		count += result & 1;
+		result >>= 1;
+	}
+
+	return (count);
+}
