4834eb06fc36f114fa05ad625cf3c6464d3f48da
xhuve <xhuvelialbana@gmail.com>
2024-04-08 13:15:35 +0200

    Fix ex.3 cases

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -11,8 +11,14 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int size = 0;
+
+	size = sizeof(n) * 8;
+	if (index > size)
+		return (-1);
+
 
 	*n = *n | (1 << index);
 
-	return (*n);
+	return (1);
 }
