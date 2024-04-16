ef14d6f62bfa83e9e91dd34b017663f4732509d4
xhuve <xhuvelialbana@gmail.com>
2024-04-16 11:45:45 +0200

    Finish 1

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int openfd, readfd, writefd;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	openfd = open(filename, O_RDONLY);
+	if (openfd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	readfd = read(openfd, buff, letters);
+	if (readfd == -1)
+		return (0);
+
+	writefd = write(STDOUT_FILENO, buff, readfd);
+	if (writefd == -1)
+		return (0);
+
+	free(buff);
+
+	close(openfd);
+
+	return (readfd);
+}
