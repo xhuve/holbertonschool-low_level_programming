b04cec3b77e1d9fdc2dd3515f823337babcc49fd
xhuve <xhuvelialbana@gmail.com>
2024-04-16 13:00:27 +0200

    Finish 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+/**
+* create_file - check code
+*
+* @filename: file name
+* @text_content: text
+*
+* Return: int
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int openfd, writefd, size = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content[size++] != '\0')
+
+	openfd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (openfd == -1)
+		return (-1);
+
+	writefd = write(STDOUT_FILENO, text_content, size);
+	if (writefd == -1)
+		return (-1);
+
+	close(openfd);
+
+	return (1);
+}
