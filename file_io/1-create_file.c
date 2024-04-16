632c8e1511b4a7ad8ae9f4ddba181018e5b5c88b
xhuve <xhuvelialbana@gmail.com>
2024-04-16 13:05:21 +0200

    Fix 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -23,10 +23,6 @@ int create_file(const char *filename, char *text_content)
 	if (openfd == -1)
 		return (-1);
 
-	writefd = write(STDOUT_FILENO, text_content, size);
-	if (writefd == -1)
-		return (-1);
-
 	close(openfd);
 
 	return (1);
