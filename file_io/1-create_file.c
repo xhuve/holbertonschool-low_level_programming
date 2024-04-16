41ac9f2a5f8d6384c2dfe2403c43039d5c57c315
xhuve <xhuvelialbana@gmail.com>
2024-04-16 13:14:20 +0200

    Write to fd rather than output

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,7 +12,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int openfd, size = 0;
+	int openfd, writefd, size = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,6 +21,11 @@ int create_file(const char *filename, char *text_content)
 
 	openfd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (openfd == -1)
+		return (-1);
+
+
+	writefd = write(openfd, text_content, size);
+	if (writefd == -1)
 		return (-1);
 
 	close(openfd);
