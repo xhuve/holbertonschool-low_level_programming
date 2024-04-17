57f6b92b36a8df1d818aabd6fc88e3ece151db8a
xhuve <xhuvelialbana@gmail.com>
2024-04-17 13:27:12 +0200

    Fix write and read return value

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -57,7 +57,10 @@ int main(int ac, char **av)
 
 	readfd = read(file_from, buffer, 1024);
 	if (readfd == -1)
-		return (-1);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
 	while (readfd > 0)
 	{
