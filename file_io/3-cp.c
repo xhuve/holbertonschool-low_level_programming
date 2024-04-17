f0c2f611b7625538bf80734db96655bf926bfda0
xhuve <xhuvelialbana@gmail.com>
2024-04-17 13:25:52 +0200

    Fix 3 error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -51,7 +51,7 @@ int main(int ac, char **av)
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 
@@ -64,13 +64,13 @@ int main(int ac, char **av)
 		writefd = write(file_to, buffer, readfd);
 		if (writefd == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 		readfd = read(file_from, buffer, 1024);
 		if (readfd == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 	}
@@ -78,7 +78,7 @@ int main(int ac, char **av)
 	closefd = close(file_from);
 	if (closefd == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
