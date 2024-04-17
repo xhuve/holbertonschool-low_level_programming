e172f7c34431dcf5300cc2f3576f27d27e930d39
xhuve <xhuvelialbana@gmail.com>
2024-04-17 13:24:39 +0200

    Fix 3 error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -70,7 +70,7 @@ int main(int ac, char **av)
 		readfd = read(file_from, buffer, 1024);
 		if (readfd == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 	}
